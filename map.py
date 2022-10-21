8c041444729973ec57752c80f98ec8e6e65654f9
donrbite <dbrite@uncc.edu>
2022-10-21 13:52:07 -0400

    added test data

--- map.py
+++ map.py
@@ -1,5 +1,12 @@
 import folium
 
+from folium.plugins import HeatMap
+
 map = folium.Map(location=[35.303555, -80.73238], tiles="OpenStreetMap", zoom_start = 15)
 
+data = [
+    [35.307634, -80.733623]
+]
+
+HeatMap(data).add_to(map)
 map.save("folium.html")
