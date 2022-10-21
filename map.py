9df21c9fe41d6f95e24d4db130c54842395c907e
donrbite <dbrite@uncc.edu>
2022-10-21 13:42:41 -0400

    fixed zoom

--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 import folium
 
-map = folium.Map(location=[35.303555, -80.73238], tiles="OpenStreetMap", zoom_start=6)
+map = folium.Map(location=[35.303555, -80.73238], tiles="OpenStreetMap", zoom_start = 15)
 
 map.save("folium.html")
