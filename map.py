3e414170d3aa01456bc55105dac12aad4d9fc4bc
Charankio <araygoza@uncc.edu>
2022-10-25 10:18:52 -0400

    Updating: Contact page,  heatmap , index video

--- map.py
+++ map.py
@@ -2,10 +2,15 @@ import folium
 
 from folium.plugins import HeatMap
 
-map = folium.Map(location=[35.303555, -80.73238], tiles="OpenStreetMap", zoom_start = 15)
+map = folium.Map(location=[35.30742034864125, -80.73590951022354], tiles="OpenStreetMap", zoom_start = 15)
 
 data = [
-    [35.307634, -80.733623]
+   [35.307515862770785, -80.73571705830427], # Woodward Hall
+   [35.31267640715162, -80.74200998338922], # Bioinformatics
+   [35.30550548981937, -80.72873019682216], # Cato
+   [35.30589304115362, -80.73214474067815], #J. Murrey Atkins Library
+   [35.3077865349813, -80.73119368119363], # Cameron
+   [35.31052012671641, -80.72956218797971] # Student Health Center
 ]
 
 HeatMap(data).add_to(map)
