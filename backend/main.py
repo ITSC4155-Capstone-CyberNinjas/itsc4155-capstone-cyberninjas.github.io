c1bcf74492e1e73908b2050a2fdc150aa6dc2583
calvinhathcock <calvinhathcock@yahoo.com>
2022-11-26 23:08:33 -0500

    notes and beginning api skeleton

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@
         uvicorn main:app
 """
 
+from datetime import datetime 
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,8 +14,18 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    pass
 
-@app.get("/map")
-async def test():
-    return {"message": "Test returned"}
+# should these "get" or "post" methods? The end points are 
+@app.post("/wifi/map")
+async def generate_wifi_map(date: datetime): # TODO: Python Datetime supported 
+    return {'date': date}
+
+@app.get("/bus/map")
+async def generate_bus_map():
+    pass
+
+@app.get("/db")
+async def test_db_query():
+    # call db here
+    pass 
