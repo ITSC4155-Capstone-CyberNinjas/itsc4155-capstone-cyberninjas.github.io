4098782d7d7a21bb0e06c9d53b2e1fd78c9b058a
Calvin Hathcock <calvinhathcock@yahoo.com>
2022-11-30 10:09:39 -0500

    file clean up

--- backend/.ipynb_checkpoints/database-checkpoint.py
+++ backend/.ipynb_checkpoints/database-checkpoint.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # TODO: read string from env variable
-engine = create_engine('mysql+mysqlconnector://root:parvardigar1@localhost:3306/CyberNinjas...') 
+engine = create_engine('mysql+mysqlconnector://root:pw@localhost:3306/CyberNinjas...') 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
